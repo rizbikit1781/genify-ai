8286ff2d3df208846e228528d305200cac876e67
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-06-20 19:57:09 -0600

    Implemented Hero section

---
+++ src/components/CompanyLogo.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { companyLogos } from '../constants'
+
+const CompanyLogo = ({ className }) => {
+  return (
+    <div className={className}>
+        <h5>Helping people create incredible content at</h5>
+        <ul className='flex'>
+            {companyLogos.map((logo, index) => (
+                <li className='flex items-center justify-center flex-1 h-[8.5rem]' key={index}>
+                    <img 
+                        src={logo}
+                        width={134}
+                        height={28}
+                        alt={logo}
+                    />
+                </li>
+            ))}
+        </ul>
+    </div>
+    
+  )
+}
+
+export default CompanyLogo
