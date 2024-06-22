c963aed6286477d31625c1c1eeae28a03167a4e9
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-06-22 16:36:36 -0600

    Implemented the benefits section

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Section from './Section'
+import Heading from './Heading'
+
+const Benefits = () => {
+  return (
+    <Section id="features">
+        <div className='container relative z-2'>
+            <Heading 
+                className="md:max-w-md lg:max-w-2xl"
+                title="Chat Smarter, Not Harder with Genify AI"
+            />
+        </div>
+        
+    </Section>
+  )
+}
+
+export default Benefits
