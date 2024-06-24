4cf3fb214c6c80926e6915328078000edab5b170
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-06-24 01:00:27 -0600

    Developed services part

---
+++ src/components/Services.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Section from './Section'
+import Heading from './Heading'
+import { check, service1, service2, service3 } from '../assets'
+import { brainwaveServices } from '../constants'
+
+const Services = () => {
+  return (
+    <Section id="how-to-use">
+      <div className='container'>
+        <Heading 
+          title="Generative AI made for creators."
+          text="Genify AI unlocks the potential of AI-powered applications"
+        />
+
+        <div className='relative'>
+          <div className='relative z-1 flex items-center h-[39rem] mb-5 p-8 border border-n-1/10 rounded-3xl overflow-hidden lg:p-20 xl:h-[46rem]'>
+            <div className='absolute w-full h-full top-0 left-0 pointer-events-none md:w-3/5 xl:w-auto'>
+              <img 
+                src={service1}
+                className='w-full h-full object-cover md:object-right'
+                width={800}
+                height={730}
+                alt="AI"
+              />
+            </div>
+
+            <div className='relative z-1 max-w-[17rem] ml-auto'>
+              <h4 className='h4 mb-4'>Smartest AI</h4>
+              <p className='body-2 mb-[3rem] text-n-3'>Genify AI unlocks the potential of AI-powered applications</p>
+              <ul className='body-2'>
+                {brainwaveServices.map((item, index) => (
+                  <li key={index} className='flex items-start py-4 border-t border-n-6'>
+                    <img src={check}
+                      width={24}
+                      height={24}
+                      alt="check"
+                    />
+                    <p className='ml-4'>{item}</p>
+                  </li>
+                ))}
+              </ul>
+            
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </Section>
+  )
+}
+
+export default Services
