49034c58396c0dc8d6cbf8e22fddca7ccf4be2f2
rizbikit1781 <112736862+rizbikit1781@users.noreply.github.com>
2024-06-27 23:24:32 -0600

    Update Services.jsx

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -75,18 +75,35 @@ const Services = () => {
                       {brainwaveServicesIcons.map((item, index) => (
                         <li 
                           key={index}
-                          className={`rounded-2xl flex items-center justify-center ${index === 2 ? 'w-[3rem] h-[3rem] p-0.3 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]': 'flex w-10 h-10 bg-n-6 md:w-15 md:h-15'}`}>
-                          <div>
+                          className={`rounded-2xl flex items-center justify-center ${index === 2 ? 'w-[3rem] h-[3rem] p-0.25 bg-conic-gradient md:w-[4.5rem] md:h-[4.5rem]': 'flex w-10 h-10 bg-n-6 md:w-15 md:h-15'}`}>
+                          <div className={index === 2  ? 'flex items-center justify-center w-full h-full bg-n-7 rounded-[1rem]' : ''}>
                             <img 
                               src={item}
+                              width={24}
+                              height={24}
+                              alt={item}
                             />
                           </div>
                         </li>
                       ))}
 
                     </ul>
-                  
                   </div>
+
+                  <div className='relative'>
+                      <img 
+                      src={service3} className='w-full h-full object-cover' 
+                      width={520}
+                      height={400}
+                      alt={service3}
+                      />
+
+                      <VideoChatMessage />
+                      <VideoBar />
+                  </div>
+
+
+
                 </div>  
 
           </div>
