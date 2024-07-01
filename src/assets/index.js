9e6fbd536edd9e7738de2bd4bf107652da35a1c2
SunsabParadon <112736862+rizbikit1781@users.noreply.github.com>
2024-07-01 15:44:54 -0600

    Completed the project

--- src/assets/index.js
+++ src/assets/index.js
@@ -10,6 +10,13 @@ import grid from "./grid.png";
 import check2 from "./check-02.svg";
 import loading1 from "./loading-01.svg";
 import yourlogo from "./yourlogo.svg";
+
+import amazon from "./amazon.svg"
+import apple from "./apple.svg"
+import epicgames from "./epicgames.svg"
+import microsoft from "./microsoft.svg"
+import openai from "./openai.svg"
+
 import homeSmile from "./home-smile.svg";
 import file02 from "./file-02.svg";
 import searchMd from "./search-md.svg";
@@ -146,4 +153,9 @@ export {
   instagram,
   telegram,
   twitter,
+  amazon,
+  apple,
+  epicgames,
+  microsoft,
+  openai
 };
--- src/constants/index.js
+++ src/constants/index.js
@@ -34,6 +34,11 @@ import {
     telegram,
     twitter,
     yourlogo,
+    amazon,
+    apple,
+    epicgames,
+    microsoft,
+    openai
   } from "../assets";
   
   export const navigation = [
@@ -75,7 +80,7 @@ import {
   
   export const notificationImages = [notification4, notification3, notification2];
   
-  export const companyLogos = [yourlogo, yourlogo, yourlogo, yourlogo, yourlogo];
+  export const companyLogos = [amazon, apple, epicgames, microsoft, openai];
   
   export const brainwaveServices = [
     "Photo generating",
@@ -96,7 +101,7 @@ import {
       id: "0",
       title: "Voice recognition",
       text: "Enable the chatbot to understand and respond to voice commands, making it easier for users to interact with the app hands-free.",
-      date: "May 2023",
+      date: "June 2024",
       status: "done",
       imageUrl: roadmap1,
       colorful: true,
@@ -105,7 +110,7 @@ import {
       id: "1",
       title: "Gamification",
       text: "Add game-like elements, such as badges or leaderboards, to incentivize users to engage with the chatbot more frequently.",
-      date: "May 2023",
+      date: "June 2024",
       status: "progress",
       imageUrl: roadmap2,
     },
@@ -113,7 +118,7 @@ import {
       id: "2",
       title: "Chatbot customization",
       text: "Allow users to customize the chatbot's appearance and behavior, making it more engaging and fun to interact with.",
-      date: "May 2023",
+      date: "June 2024",
       status: "done",
       imageUrl: roadmap3,
     },
@@ -121,7 +126,7 @@ import {
       id: "3",
       title: "Integration with APIs",
       text: "Allow the chatbot to access external data sources, such as weather APIs or news APIs, to provide more relevant recommendations.",
-      date: "May 2023",
+      date: "June 2024",
       status: "progress",
       imageUrl: roadmap4,
     },
