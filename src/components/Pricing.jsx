f41902721d7d5520d6d808756f12117634853b8a
SunsabParadon <112736862+rizbikit1781@users.noreply.github.com>
2024-06-30 20:42:01 -0600

    Implement pricing section

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Section from './Section'
 import { smallSphere, stars } from '../assets'
+import Heading from './Heading'
+import PricingList from './PricingList'
+import { LeftLine, RightLine } from './design/Pricing'
 
 const Pricing = () => {
   return (
-    <Section id="pricing">
+    <Section className="overflow-hidden" id="pricing">
         <div className='container relative z-2'>
             <div className='hidden relative justify-center mb-[6.5rem] lg:flex'>
                 <img 
@@ -13,9 +17,19 @@ const Pricing = () => {
                     height={255}
                     alt="Sphere"
                 />
-                <div>
-                    <img src={stars} />
+                <div className='absolute top-1/2 left-1/2 w-[60rem] -translate-x-1/2 -translate-y-1/2 pointer-events-none'>
+                    <img src={stars} className='w-full' width={950} height={400} alt='stars'/>
                 </div>
+            </div>
+            <Heading 
+                tag="Get started with Genify AI"
+                title="Pay once, use forever"
+            />
+
+            <div className='relative'>
+                <PricingList />
+                <LeftLine />
+                <RightLine />
             </div>
         </div>
     </Section>
